8ec711bf5ccbe1d1daace3e3d1cac430c3870648
Mazen Shalaby <waleedmazen85@gmail.com>
2026-04-20 23:03:21 +0200

    (feat): added db models, registering them in admin and migrating

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,47 @@
 from django.contrib import admin
 
+from .models import Member, Trainer, Branch, GymClass, Equipment
+
 # Register your models here.
 
 
+@admin.register(Member)
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ("name", "balance", "created_at", "updated_at")
+    search_fields = ("name",)
+    list_filter = ("name", "created_at", "updated_at")
+
+
+@admin.register(Trainer)
+class TrainerAdmin(admin.ModelAdmin):
+    list_display = ("name", "specialization", "created_at", "updated_at")
+    search_fields = ("name", "specialization")
+    list_filter = ("name", "specialization", "created_at", "updated_at")
+
+
+@admin.register(Branch)
+class BranchAdmin(admin.ModelAdmin):
+    list_display = ("name", "location", "created_at", "updated_at")
+    search_fields = ("name", "location")
+    list_filter = ("name", "created_at", "updated_at")
+
+
+@admin.register(GymClass)
+class GymClassAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "base_price",
+        "start_date",
+        "trainer",
+        "created_at",
+        "updated_at",
+    )
+    search_fields = ("title", "trainer")
+    list_filter = ("title", "start_date", "created_at", "updated_at")
+
+
+@admin.register(Equipment)
+class EquipmentAdmin(admin.ModelAdmin):
+    list_display = ("name", "is_damaged", "created_at", "updated_at")
+    search_fields = ("name",)
+    list_filter = ("name", "is_damaged", "created_at", "updated_at")
--- main/models.py
+++ main/models.py
@@ -3,3 +3,47 @@ from django.db import models
 # Create your models here.
 
 
+class CreatedUpdatedTimeStamp(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    class Meta:
+        abstract = True  
+
+class Member(CreatedUpdatedTimeStamp):
+    name = models.CharField(max_length=255, null=True, blank=False)
+    balance = models.FloatField(null=True, blank=False)
+
+    def __str__(self):
+        return self.name
+    
+class Trainer(CreatedUpdatedTimeStamp):
+    name = models.CharField(max_length=255, null=True, blank=False)
+    specialization = models.CharField(max_length=50, null=True, blank=False)
+
+    def __str__(self):
+        return self.name
+    
+
+class Branch(CreatedUpdatedTimeStamp):
+    name = models.CharField(max_length=50, null=True, blank=False)
+    location = models.CharField(max_length=50, null=True, blank=False)
+    
+    def __str__(self):
+        return self.name
+    
+class GymClass(CreatedUpdatedTimeStamp):
+    title = models.CharField(max_length=50, null=True, blank=False)
+    base_price = models.FloatField(null=True, blank=False)
+    start_date = models.DateTimeField(auto_now_add=True, null=True, blank=False)
+    trainer = models.ForeignKey(Trainer, on_delete=models.SET_DEFAULT, default="No trainer added yet for this gym class", related_name="gymclass_trainer")
+    members = models.ManyToManyField(Member, related_name="gymclass_members")
+
+    def __str__(self):
+        return self.title
+    
+    
+class Equipment(CreatedUpdatedTimeStamp):
+    name = models.CharField(max_length=255, null=True, blank=False)
+    is_damaged = models.BooleanField(default=False, null=True, blank=False)
+    branch = models.ManyToManyField(Branch, related_name="equipment_branch", null=True, blank=False)
+    
