0288c6404000eefd0c3ef250698fca7872c03f10
Mazen Shalaby <waleedmazen85@gmail.com>
2026-04-21 23:36:02 +0200

    (feat): customized trending queryset add

--- main/models.py
+++ main/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.db.models import Count
 # Create your models here.
 
 
@@ -35,16 +35,29 @@ class Branch(CreatedUpdatedTimeStamp):
     def __str__(self):
         return self.name
     
+class GymClassQuerySet(models.QuerySet):
+    def trending(self):
+        return self.annotate(count=Count("members")).filter(count=15)
+
+class GymClassManager(models.Manager):
+    def get_queryset(self):
+        return GymClassQuerySet(self.model, using=self._db).trending()
+
+    def trending(self):
+        return self.get_queryset()
+
+
 class GymClass(CreatedUpdatedTimeStamp):
     title = models.CharField(max_length=50, null=True, blank=False)
     base_price = models.FloatField(null=True, blank=False)
     start_date = models.DateTimeField(auto_now_add=True, null=True, blank=False)
     trainer = models.ForeignKey(Trainer, on_delete=models.SET_DEFAULT, default="No trainer added yet for this gym class", related_name="gymclass_trainer")
-    members = models.ManyToManyField(Member, related_name="gymclass_members")
+    members = models.ManyToManyField(Member)
 
     def __str__(self):
         return self.title
     
+    objects = GymClassManager()
     
 class Equipment(CreatedUpdatedTimeStamp):
     name = models.CharField(max_length=255, null=True, blank=False)
