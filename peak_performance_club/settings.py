cc442ce1572a65f5648291dd4d52b207b187bbc9
Mazen Shalaby <waleedmazen85@gmail.com>
2026-04-22 14:13:14 +0200

    (chore): added 'django_extensions' 3rd party package config.

--- peak_performance_club/settings.py
+++ peak_performance_club/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # 3rd party packages
+    "django_extensions",
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
